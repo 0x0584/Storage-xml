555ff00ca005d4ac2976a3f6842638968aa53d12
Anas Rchid <rchid.anas@gmail.com>
2017-04-06 01:04:50 +0000

    ADD: StorageForm

--- MagApp/StorageForm.Designer.cs
+++ MagApp/StorageForm.Designer.cs
@@ -34,9 +34,10 @@
             this.listadded = new System.Windows.Forms.ListBox();
             this.numquantity = new System.Windows.Forms.NumericUpDown();
             this.btnaddtolist = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnremove = new System.Windows.Forms.Button();
             this.lablquant = new System.Windows.Forms.Label();
             this.labltotal = new System.Windows.Forms.Label();
+            this.labnotif = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvstorage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numquantity)).BeginInit();
             this.SuspendLayout();
@@ -57,6 +58,7 @@
             this.btnconfirm.TabIndex = 1;
             this.btnconfirm.Text = "Confirm";
             this.btnconfirm.UseVisualStyleBackColor = true;
+            this.btnconfirm.Click += new System.EventHandler(this.btnconfirm_Click);
             // 
             // combproducts
             // 
@@ -65,6 +67,7 @@
             this.combproducts.Name = "combproducts";
             this.combproducts.Size = new System.Drawing.Size(137, 21);
             this.combproducts.TabIndex = 2;
+            this.combproducts.SelectedIndexChanged += new System.EventHandler(this.combproducts_SelectedIndexChanged);
             // 
             // listadded
             // 
@@ -73,10 +76,16 @@
             this.listadded.Name = "listadded";
             this.listadded.Size = new System.Drawing.Size(137, 212);
             this.listadded.TabIndex = 3;
+            this.listadded.SelectedIndexChanged += new System.EventHandler(this.listadded_SelectedIndexChanged);
             // 
             // numquantity
             // 
             this.numquantity.Location = new System.Drawing.Point(37, 49);
+            this.numquantity.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
             this.numquantity.Name = "numquantity";
             this.numquantity.Size = new System.Drawing.Size(109, 20);
             this.numquantity.TabIndex = 4;
@@ -91,14 +100,15 @@
             this.btnaddtolist.UseVisualStyleBackColor = true;
             this.btnaddtolist.Click += new System.EventHandler(this.btnaddtolist_Click);
             // 
-            // button1
+            // btnremove
             // 
-            this.button1.Location = new System.Drawing.Point(28, 303);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(101, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Remove From List";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnremove.Location = new System.Drawing.Point(28, 303);
+            this.btnremove.Name = "btnremove";
+            this.btnremove.Size = new System.Drawing.Size(101, 23);
+            this.btnremove.TabIndex = 5;
+            this.btnremove.Text = "Remove From List";
+            this.btnremove.UseVisualStyleBackColor = true;
+            this.btnremove.Click += new System.EventHandler(this.btnremove_Click);
             // 
             // lablquant
             // 
@@ -113,28 +123,39 @@
             // labltotal
             // 
             this.labltotal.AutoSize = true;
-            this.labltotal.ForeColor = System.Drawing.Color.Maroon;
+            this.labltotal.ForeColor = System.Drawing.Color.DarkBlue;
             this.labltotal.Location = new System.Drawing.Point(232, 18);
             this.labltotal.Name = "labltotal";
             this.labltotal.Size = new System.Drawing.Size(42, 13);
             this.labltotal.TabIndex = 6;
             this.labltotal.Text = "TOTAL";
             // 
-            // Storage
+            // labnotif
+            // 
+            this.labnotif.AutoSize = true;
+            this.labnotif.ForeColor = System.Drawing.Color.Maroon;
+            this.labnotif.Location = new System.Drawing.Point(175, 313);
+            this.labnotif.Name = "labnotif";
+            this.labnotif.Size = new System.Drawing.Size(89, 13);
+            this.labnotif.TabIndex = 7;
+            this.labnotif.Text = "NOTIFICATIONS";
+            // 
+            // StorageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(418, 333);
+            this.Controls.Add(this.labnotif);
             this.Controls.Add(this.labltotal);
             this.Controls.Add(this.lablquant);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnremove);
             this.Controls.Add(this.btnaddtolist);
             this.Controls.Add(this.numquantity);
             this.Controls.Add(this.listadded);
             this.Controls.Add(this.combproducts);
             this.Controls.Add(this.btnconfirm);
             this.Controls.Add(this.dgvstorage);
-            this.Name = "Storage";
+            this.Name = "StorageForm";
             this.Text = "Storage";
             ((System.ComponentModel.ISupportInitialize)(this.dgvstorage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numquantity)).EndInit();
@@ -151,8 +172,9 @@
         private System.Windows.Forms.ListBox listadded;
         private System.Windows.Forms.NumericUpDown numquantity;
         private System.Windows.Forms.Button btnaddtolist;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnremove;
         private System.Windows.Forms.Label lablquant;
         private System.Windows.Forms.Label labltotal;
+        private System.Windows.Forms.Label labnotif;
     }
 }
--- MagApp/StorageForm.cs
+++ MagApp/StorageForm.cs
@@ -11,23 +11,149 @@ namespace MagApp
 {
     public partial class StorageForm : Form
     {
-        List<Product> products;
+
+        Product current;
 
         public StorageForm()
         {
             InitializeComponent();
-            products = (List<Product>) Product.List;
 
-            foreach (Product prod in products)
-            {
+            foreach (Product prod in Product.List)
+                combproducts.Items.Add(prod.Lable);
 
-            }
+            current = new Product();
+
+            combproducts.Text = combproducts.Items[0].ToString();
         }
 
         private void btnaddtolist_Click(object sender, EventArgs e)
         {
+            // TODO: fix the add in the list
+            //
+
+            bool exists = false;
+            decimal newvalue = 0;
+            string newstring = "";
+            int i = 0;
+
+            foreach (string item in listadded.Items)
+            {
+                string[] str = item.ToString().Split(new char[2] { '(', ')' });
+
+                if (string.Equals(current.Lable, str[0].Substring(0, str[0].Length - 1)))
+                {
+                    newvalue = numquantity.Value + int.Parse(str[1]);
+                    newstring = string.Format("{0} ({1})", current.Lable, (numquantity.Value = newvalue));
+                    exists = true; break;
+                }
+                ++i;
+            }
+
+            if (!exists)
+            {
+                listadded.Items.Add(string.Format("{0} ({1})", current.Lable, numquantity.Value));
+                listadded.SetSelected(listadded.Items.Count - 1, true);
+            }
+            else
+            {
+                listadded.Items[i] = newstring;
+                listadded.SetSelected(i, true);
+            }
+            
+        }
+
+        private void btnremove_Click(object sender, EventArgs e)
+        {
+            if (listadded.SelectedItem != null)
+            {
+                int index = listadded.SelectedIndex;
+                string[] str = listadded.Items[index].ToString().Split(new char[] { '(',')'});
+                
+                // the total taht is stored in teh label
+                float total = int.Parse(labltotal.Text.Split(new char[] { ' ' })[0]),
+                price = 0.0f;
+
+                foreach (Product prod in Product.List)
+                    if (string.Equals(prod.Lable, str[0]))
+                        price = prod.Price;
+
+                // minus teh price of the removed items
+                total -= (float.Parse(str[1]) * price);
+
+                labltotal.Text = total.ToString();
+                listadded.Items.Remove(listadded.Items[index]);
+            }
+            else { labnotif.Text = "Select a product first"; }
+        }
+
+        private void btnconfirm_Click(object sender, EventArgs e)
+        {
+            // TODO: apply things to the dgv 
+            //
+            
+            foreach (string item in listadded.Items)
+            {
+                string[] str = item.Split(new char[] { '(', ')' });
+
+                foreach (Product prod in Product.List)
+                {
+                     
+                }
+            }
+
+            // TODO: apply things to the xml file 
+            //
+
+             
+        }
+
+        private void listadded_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            labnotif.Text = "";
+
+            if (listadded.SelectedItem != null)
+            {
+                string[] info = listadded.SelectedItem.ToString().Split(new char[2] { '(', ')' });
+
+                combproducts.Text = info[0];
+                numquantity.Value = int.Parse(info[1]);
+            }
+
+            float total = 0;
+
+            // TODO: find how to take few digits from 
+            // the foalt number
+            // 
+
+            foreach (string item in listadded.Items)
+                foreach (Product prod in Product.List)
+                {
+                    string[] str = item.ToString().Split(new char[2] { '(', ')' });
+
+                    if (string.Equals(str[0].Substring(0, str[0].Length - 1), prod.Lable))
+                    {
+                        total += (float.Parse(str[1]) * prod.Price);
+                        break;
+                    }
+                }
+
+            labltotal.Text = string.Format("{0:0.00} MAD" , total.ToString());
 
         }
 
+        private void combproducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // TODO: update the quantity lable
+            // done.
+
+            string labcurrent = combproducts.SelectedItem.ToString();
+
+            foreach (Product prod in Product.List)
+                if (prod.Lable == labcurrent)
+                { prod.CopyTo(current); break; }
+
+            lablquant.Text = string.Format("({0})", current.Quantity);
+        }
     }
 }
+
